029c3dd74dcc46838f72053c79c75d8fd1d376c5
emirhan <rza.emirhan@gmail.com>
2024-01-09 01:38:56 +0300

    first db

--- server2.js
+++ server2.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Product = require('./models/productModel')
 const app = express()
+
+app.use(express.json())
 
 //routes
 
@@ -10,6 +13,17 @@ app.get('/', (req, res) => {
 
 app.get('/blog', (req, res) => { 
     res.send('Hello Blog')
+})
+
+app.post('/product', async(req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product)
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
 })
 
 mongoose.set("strictQuery", false)
