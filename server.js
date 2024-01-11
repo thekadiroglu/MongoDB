fa284e8eca9a453fae99385581475ad172fc46b1
emirhan <rza.emirhan@gmail.com>
2024-01-12 01:07:59 +0300

    UpdateDB

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors');
 const Product = require('./models/productModel')
 const app = express()
 
-
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 //routes
